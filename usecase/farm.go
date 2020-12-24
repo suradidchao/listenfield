7f20f7fbbd71e7f9f2200d89de9802e19a96810c
Suradid Chao <chill_shyld@hotmail.com>
2020-12-24 14:07:11 +0700

    add farm usecase

--- usecase/farm.go
+++ usecase/farm.go
@@ -2,14 +2,22 @@ package usecase
 
 import (
 	"github.com/suradidchao/listenfield/entity"
+	"github.com/suradidchao/listenfield/repo/farm"
 )
 
 // Farm is a usecase for farm
 type Farm struct {
-	farmRepo entity.Farm
+	farmRepo farm.IRepo
 }
 
 // Create is a create farm usecase
-func (f Farm) Create(farm Farm, farmer entity.Farmer) (fid int, err error) {
-	return
+func (f Farm) Create(farm entity.Farm, farmerID int) (fid int, err error) {
+	return f.farmRepo.CreateFarm(farm, farmerID)
+}
+
+// NewFarm is a factory method for farm usecase
+func NewFarm(fr farm.IRepo) Farm {
+	return Farm{
+		farmRepo: fr,
+	}
 }
